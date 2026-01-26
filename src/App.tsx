501318215e833071388cac73195be93fd3720e5b
fpothenick <fpothenick@github.com>
2026-01-26 20:49:04 +0100

    Add ScrollToTop component and update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Welcome from './pages/Welcome';
 import DateSelection from './pages/DateSelection';
 import Home from './pages/Home';
 import PackageDetail from './pages/PackageDetail';
+import ScrollToTop from './components/ScrollToTop';
 import './styles/global.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/dates" element={<DateSelection />} />
